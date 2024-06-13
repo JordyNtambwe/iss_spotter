e45792789156c6886d2759a9d154432fd31b8f91
JordyNtambwe <152664833+JordyNtambwe@users.noreply.github.com>
2024-06-13 00:09:28 -0600

    Update iss.js

--- iss.js
+++ iss.js
@@ -1,1 +1,17 @@
+const needle = require('needle');
 
+const fetchMyIP = function(callback) {
+  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = body.ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
